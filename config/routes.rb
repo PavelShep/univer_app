e68919ea3c095b72d842fef8683908c6ae178bca
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-08-11 13:51:50 +0200

    add basic staff

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cources#index'
+  get 'about', to: 'pages#about'
+  get 'cources/new', to: 'cources#new'
 end
