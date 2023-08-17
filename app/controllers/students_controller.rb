d77e70b2507b9a2fd8297f689c758c59eaa78252
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-08-17 15:02:46 +0200

    add action edit, update and view edit for Student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -21,6 +21,20 @@ class StudentsController < ApplicationController
     end
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    if @student.update(student_params)
+      flash[:notice] = "You have succefully updated profile info"
+      redirect_to student_path(@student)
+    else 
+      render 'edit'
+    end
+  end
+
   private
   
   def student_params
