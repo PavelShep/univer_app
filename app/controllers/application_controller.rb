5ba127fb2b413fb1ac8891a941577b0bffc9d6e9
Pavel Shapialevich <Pavel.Shapialevich@student.ajp.edu.pl>
2023-08-18 17:11:11 +0200

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  # to be visible also in the views
+  helper_method :current_user, :logged_in?
+
+  # to be visible in the controllers
+  def current_user
+    @current_user ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in?
+    !!current_user # convert to bool
+  end
+
+  def require_user
+    if !logged_in?
+      flash[:alert] = "You must be logged in to perform that action"
+      redirect_to login_path
+    end
+  end
 end
